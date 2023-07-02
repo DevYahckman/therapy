f12a4f8ba65fd5110c31defc70fd9dbcdbfc5b39
Yahckman <adeniranajibade2014@gmail.com>
2023-07-02 02:59:45 -0700

    setting

--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -1,77 +1,82 @@
 import React from "react";
 import styles from "./header.module.scss";
-import { Link } from 'react-router-dom';
-import { AiFillHome, AiOutlineMenuUnfold } from "react-icons/ai";
-import {CgMenuGridR} from 'react-icons/cg'
+import { Link } from "react-router-dom";
+import { CgMenuGridR } from "react-icons/cg";
 import { useState } from "react";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import { navItems } from "./navItem";
 
-import Offcanvas from 'react-bootstrap/Offcanvas';
-import { FcServices} from 'react-icons/fc';
-import {BsInfoCircle} from 'react-icons/bs'
-import {MdContactMail, MdPriceChange} from 'react-icons/md'
-import {IoCarSportSharp} from 'react-icons/io5'
-import {GrBlog} from 'react-icons/gr'
 function Header(props) {
-  const [scrollValue, setScrollValue]= useState(window.scrollY)
-    const [isActive, setisActive] = useState(false)
-    const [show, setShow]=useState(false);
+  const [isActive, setisActive] = useState(false);
+  const [show, setShow] = useState(false);
 
-    const handleShow=()=>{setShow(true)}
-    const handleclose=()=>{setShow(false)}
-    const handleActive=()=>{
-     
-        setisActive(!isActive)
-    }
+  const handleShow = () => {
+    setShow(true);
+  };
+  const handleclose = () => {
+    setShow(false);
+  };
+  const handleActive = () => {
+    setisActive(!isActive);
+  };
   return (
     <div>
       <div className={` container ${styles.header}`}>
-        <div className={styles.title}>
-          <span className={styles.title_main}>Car</span>
-          <span className= {styles.title_sub}>Book</span>
-        </div>
+        <div className={styles.title}>Mediplus</div>
 
-        <nav  className={`  ${styles.hideOnMobile} ${styles.navs}`}>
-            <Link to='/'  className={` ${styles.nav_items}`}>Home</Link>
-            <Link to='#'  className={` ${styles.nav_items}`}>About</Link>
-            <Link to='#'  className={` ${styles.nav_items}`}>Department</Link>
-            <Link to='#'  className={` ${styles.nav_items}`}>Blog</Link>
-            <Link to='#'  className={` ${styles.nav_items}`}>Contact</Link>
-            <Link to='/appointment'  className={`${styles.btn} ${styles.nav_items}`}>Appointment</Link>
+        <nav className={`  ${styles.hideOnMobile} ${styles.navs}`}>
+          <Link to="/" className={` ${styles.nav_items}`}>
+            Home
+          </Link>
+          <Link to="#" className={` ${styles.nav_items}`}>
+            About
+          </Link>
+          <Link to="#" className={` ${styles.nav_items}`}>
+            Department
+          </Link>
+          <Link to="#" className={` ${styles.nav_items}`}>
+            Blog
+          </Link>
+          <Link to="#" className={` ${styles.nav_items}`}>
+            Contact
+          </Link>
+          <Link
+            to="/appointment"
+            className={`${styles.btn} ${styles.nav_items}`}
+          >
+            Appointment
+          </Link>
         </nav>
 
-
         <div className={`${styles.hideOnDesktop} ${styles.icon}`}>
-           <CgMenuGridR size={30} onClick={handleShow}/>
-           
-           <Offcanvas show={show} onHide={handleclose} placement={"end"} >
+          <CgMenuGridR size={30} onClick={handleShow} />
 
-           <Offcanvas.Header closeButton>
-          <Offcanvas.Title>
-          <div className={styles.title}>
-          <span className={styles.title_main}>Car</span>
-          <span className= {styles.title_sub}>Book</span>
+          <Offcanvas show={show} onHide={handleclose} placement={"end"}>
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title>
+                <div className={styles.title}>Mediplus</div>
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+
+            <Offcanvas.Body>
+              <div className={styles.resp_nav}>
+                {navItems.map((item) => (
+                  <Link
+                    to={item.path}
+                    onClick={handleclose}
+                    className={styles.resp_navs}
+                  >
+                    {" "}
+                    <span className={styles.resp_navs_logo}>
+                      {item.icon}
+                    </span>{" "}
+                    {item.label}
+                  </Link>
+                ))}
+              </div>
+            </Offcanvas.Body>
+          </Offcanvas>
         </div>
-          </Offcanvas.Title>
-        </Offcanvas.Header>
-
-        <Offcanvas.Body>
-          <div className={styles.resp_nav}>
-            <Link to='/' onClick={handleclose} className={styles.resp_navs}> <span className={styles.resp_navs_logo}><AiFillHome size={20}/></span> Home</Link>
-            <Link to='/about' onClick={handleclose} className={styles.resp_navs}> <span className={styles.resp_navs_logo}>< BsInfoCircle size={20}/></span> About</Link>
-            <Link to='/services' onClick={handleclose} className={styles.resp_navs}> <span className={styles.resp_navs_logo}><FcServices size={20}/></span> Departmen</Link>
-            <Link to='/pricing' onClick={handleclose} className={styles.resp_navs}> <span className={styles.resp_navs_logo}><MdPriceChange size={20}/></span> Blog</Link>
-            <Link to='#' onClick={handleclose} className={styles.resp_navs}>
-              <span className={styles.resp_navs_logo}>< GrBlog size={20}/></span> Blog</Link>
-            <Link to='/contact' onClick={handleclose} className={styles.resp_navs}> <span className={styles.resp_navs_logo}>< MdContactMail size={20}/></span>  Contact</Link>
-            {/* <Link to='/appointment' onClick={handleclose} className={styles.resp_navs}> <span className={styles.resp_navs_logo}>< MdContactMail size={20}/></span>  Appointment</Link> */}
-            
-          </div>
-        </Offcanvas.Body>
-           </Offcanvas>
-       </div>
-
-      
-       
       </div>
     </div>
   );
